0e867b498bd2898bad0fbd4f424aebd0d975e027
Nsikak Akpan <builtbykaksy@gmail.com>
2025-05-21 04:56:44 +0100

    push

--- Javascript/asynchronous-programming.js
+++ Javascript/asynchronous-programming.js
@@ -18,38 +18,40 @@
 
 // console.log("Task 1: Start");
 
-// const displaySomething = () => {
-//   console.log("Task 2: This is delayed by 5 seconds");
-// }
+const displaySomething = () => {
+  console.log("Task 2: This is delayed by 5 seconds");
+}
 
 // Callback function executed after 2 seconds (2000 milliseconds)
 // setTimeout(displaySomething, 5000);
 
 // setTimeout(function() {
-//   console.log("Task 2: This is delayed by 2 seconds");
+//   console.log("Task 3: This is delayed by 2 seconds");
 // }, 2000);
 
-// console.log("Task 3: End");
-// const data = { user: "Nsikak", age: 30 };
+// console.log("Task 4: End");
 
-// function fetchData(callback) {
-//   console.log("Fetching data...");
 
-//   // Simulate a delay (e.g., fetching data from an API)
-//   setTimeout(() => {
-//     const data = { user: "Musa", age: 29 };
-//     callback(data);
-//   }, 2000); // Wait 2 seconds before executing the callback
-// }
+const data = { user: "Nsikak", age: 30 };
+
+function fetchData(callback) {
+  console.log("Fetching data...");
+
+  // Simulate a delay (e.g., fetching data from an API)
+  setTimeout(() => {
+    const data = { user: "Musa", age: 29 };
+    callback(data);
+  }, 2000); // Wait 2 seconds before executing the callback
+}
 
 // console.log(data)
-// function processData(parameter) {
-//   console.log(`User: ${parameter.user}, Age: ${parameter.age}`);
-// }
+function processData(parameter) {
+  console.log(`User: ${parameter.user}, Age: ${parameter.age}`);
+}
 
-// function sayHello(data){
-//   console.log(data);
-// }
+function sayHello(data){
+  console.log(data);
+}
 
 // // Passing 'processData' as a callback to 'fetchData'
 // fetchData(processData);
@@ -109,37 +111,30 @@ const nameAndAgeAndLocationLogger = (dataObject) => {
 // getUser(nameLogger);
 
 
-// const doings = () => {
-//   setTimeout(() => {
-//     console.log('Decide what you want to eat'); 
-//     setTimeout(() => {
-//       console.log('I want to eat Indomie noodles');
-//       setTimeout(() => {
-//         console.log('I just bought three cartons of Indomie noodles');
-//         setTimeout(() => {
-//           console.log('I just placed a sauce-pan on fire');
-//           setTimeout(() => {
-//             console.log('I have added some water to the pot');
-//             setTimeout(() => {
-//               console.log('The water is boiling');
-//               setTimeout(() => {
-//                 console.log('I successfully added my noodles to the pan');
-//                 setTimeout(() => {
-//                   console.log('My Delicious pan of noodles is ready');
-//                   setTimeout(() => {
-//                     console.log('I have hurriedly and successfully finished all my Noodles')
-//                   }, 1000)
-//                 }, 5000)
-//               }, 1000)
-//             }, 4000)
-//           }, 1000)
-//         }, 1000)
-//       }, 3000)
-//     }, 2000)   
-//   }, 0)
-// }
+const cookNoodles = () => {
+ console.log('I have decided to eat noodles');
+ setTimeout(() => {
+  console.log('I just bought a carton of noodles');
+  setTimeout(() => {
+    console.log('I just lit up the cooker');
+    setTimeout(() => {
+      console.log('the water is boiling');
+      setTimeout(() => {
+        console.log('I just added the noodles');
+        setTimeout(() => {
+          console.log('i just added eggs and seasonings')
+          setTimeout(() => {
+            console.log('My delicious pot of noodles is ready, come and join me')
+          }, 5000)
+        }, 2000)
+      }, 2000)
+    }, 5000)
+  }, 2000)
+ }, 5000)
 
-// doings();
+}
+
+cookNoodles();
 
 
 // Callback Hell makes code harder to follow. Promises and async/await can help solve this problem.
@@ -366,37 +361,37 @@ const wait = (ms) => new Promise((resolve, reject) => {
   }, ms);
 });
 
-const doings = async () => {
-  try {
-    console.log('Decide what you want to chop');
-    await wait(2000);
+// const doings = async () => {
+//   try {
+//     console.log('Decide what you want to chop');
+//     await wait(2000);
 
-    console.log('I want to eat Indomie noodles');
-    await wait(3000);
+//     console.log('I want to eat Indomie noodles');
+//     await wait(3000);
 
-    console.log('I just bought three cartons of Indomie noodles');
-    await wait(1000);
+//     console.log('I just bought three cartons of Indomie noodles');
+//     await wait(1000);
 
-    console.log('I just placed a sauce-pan on fire');
-    await wait(1000);
+//     console.log('I just placed a sauce-pan on fire');
+//     await wait(1000);
 
-    console.log('I have added some water to the pot');
-    await wait(4000);
+//     console.log('I have added some water to the pot');
+//     await wait(4000);
 
-    console.log('The water is boiling');
-    await wait(1000);
+//     console.log('The water is boiling');
+//     await wait(1000);
 
-    console.log('I successfully added my noodles to the pan');
-    await wait(5000);
+//     console.log('I successfully added my noodles to the pan');
+//     await wait(5000);
 
-    console.log('My Delicious pan of noodles is ready');
-    await wait(1000);
+//     console.log('My Delicious pan of noodles is ready');
+//     await wait(1000);
 
-    console.log('I have hurriedly and successfully finished all my Noodles');
-  } catch (error) {
-    console.error('Something went wrong:', error);
-  }
-};
+//     console.log('I have hurriedly and successfully finished all my Noodles');
+//   } catch (error) {
+//     console.error('Something went wrong:', error);
+//   }
+// };
 
 // doings();
 
@@ -415,29 +410,29 @@ const doings = async () => {
 const productsContainer = document.getElementById('products-container');
 
 // Fetching a list of products from FakeStore API
-fetch('https://fakestoreapi.com/products')
-  .then((response) => response.json())
-  .then((products) => {
-    console.log('FakeStore API Products:', products);
-    products.forEach((product) => {
-      const { category, description, image, price, title} = product;
-      productsContainer.innerHTML += `
-      <div>
-            <h1>Title: ${title}</h1>
-            <h2>Category: ${category}</h2>
-            <img src=${image} alt=${title}>
-            <p>Price: ${price}</p>
-            <p>Description: ${description}</p>
-            <button>Add to Cart</button>
-        </div>
-    `
-    })
+// fetch('https://fakestoreapi.com/products')
+//   .then((response) => response.json())
+//   .then((products) => {
+//     // console.log('FakeStore API Products:', products);
+//     products.forEach((product) => {
+//       const { category, description, image, price, title} = product;
+//       productsContainer.innerHTML += `
+//       <div>
+//             <h1>Title: ${title}</h1>
+//             <h2>Category: ${category}</h2>
+//             <img src=${image} alt=${title}>
+//             <p>Price: ${price}</p>
+//             <p>Description: ${description}</p>
+//             <button>Add to Cart</button>
+//         </div>
+//     `
+//     })
     
-  })
-  .catch((error) => console.error('Error:', error));
+//   })
+//   .catch((error) => console.error('Error:', error));
 
 // Fetching a single product from FakeStore API
-fetch('https://fakestoreapi.com/products/1')
-  .then(response => response.json())
-  .then(product => console.log('FakeStore API Single Product:', product))
-  .catch(error => console.error('Error:', error));
+// fetch('https://fakestoreapi.com/products/1')
+//   .then(response => response.json())
+//   .then(product => console.log('FakeStore API Single Product:', product))
+//   .catch(error => console.error('Error:', error));
